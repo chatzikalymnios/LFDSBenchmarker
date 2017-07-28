603fbe5e84c5f4baefcc6064978053e71035b2e2
Evangelos Chatzikalymnios <chatzikalymnios@gmail.com>
2017-07-28 15:42:11 +0300

    Basic unit test

---
+++ src/test/java/io/github/chatzikalymnios/lfds/EliminationBackoffStackTest.java
@@ -0,0 +1,35 @@
+package io.github.chatzikalymnios.lfds;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EliminationBackoffStackTest {
+
+	private EliminationBackoffStack<Integer> stack;
+
+	@Before
+	public void setUp() throws Exception {
+		// Single-threaded operation
+		stack = new EliminationBackoffStack<>(1);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testPushPop() {
+		stack.push(1);
+		stack.push(2);
+
+		assertEquals((long) 2, (long) stack.pop());
+
+		stack.push(3);
+		assertEquals((long) 3, (long) stack.pop());
+		assertEquals((long) 1, (long) stack.pop());
+	}
+
+}
